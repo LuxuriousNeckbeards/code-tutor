28649e2a32ce2568b0af8fca930a1dbe0aa16978
Rane Gridley <ranegri3@gmail.com>
2016-04-19 20:04:17 -0700

    Add profile btn and desired view routes

--- client/app/nav/nav.js
+++ client/app/nav/nav.js
@@ -1,10 +1,12 @@
 angular.module('codellama.nav', [])
 
-.controller('NavController', function($scope, $rootScope, Auth, $location) {
+.controller('NavController', function($scope, $rootScope, Auth, $location, $window) {
 
   // adds bootstrap active class if path matches href value
-  $scope.isActive = function (viewLocation) { 
+  $scope.isActive = function (viewLocation) {
     return viewLocation === $location.path();
   };
+
+  $scope.username = $window.localStorage.getItem('username');
 
 });
